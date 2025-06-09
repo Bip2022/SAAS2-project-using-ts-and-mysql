2dca3bc9831be2923661d303ad376566751645cf
Bipin Ghimire <beepinghimire2022@gmail.com>
2025-06-09 18:09:27 +0545

    token generation

--- src/controller/globals/auth/auth.controller.ts
+++ src/controller/globals/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import User from '../../../database/models/user.model';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 class AuthController {
   static async registerUser(req: Request, res: Response) {
@@ -22,6 +23,40 @@ class AuthController {
         email: email
       })
       return res.status(201).json({ message: "User registered successfully" });
+    }
+  }
+  async loginUser(req:Request , res:Response){
+    const{email, password} = req.body;
+    //check if the required fields are present
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    //find the user by email
+    const data = await User.findAll({
+      where:{
+        email
+      }
+    })
+    //check if the user exists
+    if(data.length === 0){
+      return res.status(404).json({ message: "User not found" });
+    }else{
+      //check if the password is correct
+      const isPasswordMatch =  bcrypt.compareSync(password,data[0].password)
+      if(isPasswordMatch){
+       const token = jwt.sign({id :data[0].id},"dvsghvhdsghgdfhdsvh",{
+        expiresIn:'5minutes'
+       })
+       res.json({
+        token: token
+       })
+      }else{
+        return res.status(200).json({ message: "Invalid Credentials" });
+      }
+
+
+
+    
     }
   }
 }
